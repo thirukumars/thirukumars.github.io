b07d1fa3127ff2063b7e0eb240e9e5c240ad2126
PraveenKumar <praveen.senthi03@gmail.com>
2023-06-27 23:08:53 +0530

    Deploy Next.js app as static site

---
+++ out/_next/static/chunks/6728d85a-8cc77fd11121b977.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[158],{5155:function(t,e,n){n.d(e,{tzA:function(){return a}});var u=n(8357);function a(t){return(0,u.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fillRule:"evenodd",d:"M23,20 L23,6 L12,15 L1,6 L1,20 L23,20 Z M12,12 L22,4 L2,4 L12,12 Z"}}]})(t)}}}]);
