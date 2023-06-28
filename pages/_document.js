4d3d41b41b229d7c662e505ebff8559dc2ec4cf7
praveenkumars <39609315+thirukumars@users.noreply.github.com>
2023-06-28 08:43:45 +0530

    responsive meta tag

--- pages/_document.js
+++ pages/_document.js
@@ -1,3 +1,4 @@
+
 // pages/_document.js
 import { Html, Head, Main, NextScript } from "next/document";
 
@@ -5,6 +6,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
